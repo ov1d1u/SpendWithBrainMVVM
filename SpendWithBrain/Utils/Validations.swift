7a3978560a16b2da9c216d33f5bfaa65a45f2523
Ovidiu Nitan <nitanovidiu@gmail.com>
2022-11-27 23:45:13 +0200

    Added CNP validator and tests

--- SpendWithBrain/Utils/Validations.swift
+++ SpendWithBrain/Utils/Validations.swift
@@ -33,5 +33,49 @@ class Validations{
         return false
     }
     
-    
+    static func cnpValid(_ cnp: String) -> Bool {
+        func checksum(for partialNumber: String) -> Int? {
+            let checkString = "279146358279"
+            var sum = 0
+            
+            for (i, char) in partialNumber.enumerated() {
+                guard let digit = Int(String(char)) else { return nil }
+                let checkDigit = Int(String(checkString[i]))!
+                sum += digit * checkDigit
+            }
+            
+            let checksum = sum % 11
+            return checksum == 10 ? 1 : checksum
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        
+        let validSexes = ["1", "2"]
+        let validCounties = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                             "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                             "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                             "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                             "41", "42", "43", "44", "45", "46", "51", "52"]
+        
+        guard cnp.count == 13 else { return false }
+        guard cnp.isNumber else { return false }
+        
+        let sex = String(cnp[0])
+        let year = String(cnp[1...2])
+        let month = String(cnp[3...4])
+        let day = String(cnp[5...6])
+        let county = String(cnp[7...8])
+        let ordNum = String(cnp[9...11])
+        let checkNum = Int(String(cnp[12]))!
+        
+        if !validSexes.contains(sex) { return false }
+        if dateFormatter.date(from: "\(year)\(month)\(day)") == nil { return false }
+        if !validCounties.contains(county) { return false }
+        if let ordNum = Int(ordNum), ordNum == 0 && ordNum > 999 { return false }
+        guard let computedCheckNum = checksum(for: String(cnp[0...11])) else { return false }
+        if checkNum != computedCheckNum { return false }
+        
+        return true
+    }
 }
